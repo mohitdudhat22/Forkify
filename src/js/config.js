542eeae449117c4829d994a335a72f5d334729c8
mohitdudhatverselix <mohitdudhat22@gmail.com>
2024-06-28 20:56:37 +0530

    init commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ee8c2fe0-41fa-411c-b9ea-a31d9ea0bb9b';
+export const MODAL_CLOSE_SEC = 2.5;
